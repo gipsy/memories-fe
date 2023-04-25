d51edbf12399cec32ae25b0c51441dfafd7cdac2
gipsy <ter4444@gmail.com>
2023-04-25 16:48:22 +0300

    accomplished auth logic

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,12 @@
 import { AUTH } from '../constants/actionTypes';
-import * as api from '../index';
+import * as api from '../api/index';
 
 export const signin = (formData, history) => async (dispatch) => {
   try {
-    // await api.signin();
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
+
     history.push('/');
   } catch (error) {
     console.log(error);
@@ -12,7 +15,10 @@ export const signin = (formData, history) => async (dispatch) => {
 
 export const signup = (formData, history) => async (dispatch) => {
   try {
-    // await api.signup();
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+
     history.push('/');
   } catch (error) {
     console.log(error);
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -45,7 +45,7 @@ const Auth = () => {
 
   const switchMode = () => {
     setIsSignup((prevIsSignup) => !prevIsSignup);
-    handleShowPassword(false);
+    setShowPassword(false);
   }
 
   const googleLogin = useGoogleLogin({
