d4ad33b7b9770bb8f25d12af3175b22fc42b3c11
gipsy <ter4444@gmail.com>
2023-04-27 16:20:45 +0300

    added new memoriesLogo,memoriesText

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,9 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import decode from 'jwt-decode';
 
+import memoriesLogo from '../../images/memoriesLogo.png';
+import memoriesText from '../../images/memoriesText.png';
 import useStyles from './styles';
-import memories from "../../images/memories.png";
 
 
 const Navbar = () => {
@@ -39,13 +40,10 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography
-          component={Link} to="/"
-          className={classes.heading} variant="h2"
-        >
-          Memories
-        </Typography>
-        <img className={classes.image} src={memories} alt="memories" height="60" />
+        <Link to="/">
+          <img src={memoriesText} alt="icon" height="45px" />
+          <img className={classes.image} src={memoriesLogo} alt="icon" height="40" />
+        </Link>
       </div>
       <Toolbar className={classes.toolbar}>
         {user ? (
