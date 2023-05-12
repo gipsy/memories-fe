56845f145e7598676909dabb7ffbe3a42b894dcf
gipsy <ter4444@gmail.com>
2023-05-12 20:14:49 +0300

    added BE URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories-hlg7.onrender.com:10000' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
