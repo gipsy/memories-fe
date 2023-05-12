b58815d1f459d4fe5193f87488c9d277871abe04
gipsy <ter4444@gmail.com>
2023-05-12 21:04:33 +0300

    added build to repo

---
+++ build/precache-manifest.2fb0326827b151fc640a50b06193b2aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8206268d17eed04bd180f2005611d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09612b8e543b2bb7442",
+    "url": "/static/css/main.b4ff0b6c.chunk.css"
+  },
+  {
+    "revision": "71a6d2599fa6b91868bf",
+    "url": "/static/js/2.e8339a65.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.e8339a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09612b8e543b2bb7442",
+    "url": "/static/js/main.03d98c99.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.03d98c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
